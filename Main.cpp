caab2376cc0357fbe879929978b6ee9d1b28807f
MintzyG <hero.b.braga@gmail.com>
2022-10-28 03:22:50 -0300

    CPPfication e Avanço na multiplicação

--- Main.cpp
+++ Main.cpp
@@ -1,118 +1,135 @@
 #include <iostream>
-#include <cmath>
+#include <stdlib.h>
+using namespace std;
 
-void somar(int n, int m){
+    void somar(int n, int m){
 
-int valor = 0;
+    int valor = 0;
 
-int Matriz[n][m];
-int Matriz2[n][m];
+    int Matriz[n][m];
+    int Matriz2[n][m];
 
-for (int i = 0; i < m; i++){
-    for (int j = 0; j < n; j++){
-        valor += 1;
-        printf("Digite o valor %d da matriz 1: ", valor);
-        scanf("%d", &Matriz[i][j]);
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            valor += 1;
+            cout<<"Digite o valor "<< valor <<" da matriz 1: ";
+            cin>>Matriz[i][j];
+        }
     }
-}
 
-printf("\n");
+    cout<<"\n";
 
-valor = 0;
-for (int i = 0; i < m; i++){
-    for (int j = 0; j < n; j++){
-        valor += 1;
-        printf("Digite o valor %d da matriz 2: ", valor);
-        scanf("%d", &Matriz2[i][j]);
+    valor = 0;
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            valor += 1;
+            cout<<"Digite o valor "<< valor <<" da matriz 2: ";
+            cin>>Matriz2[i][j];
+        }
     }
-}
 
-printf("\n");
+    cout<<"\n";
 
-for (int i = 0; i < m; i++){
-    for (int j = 0; j < n; j++){
-        printf("%d ", Matriz[i][j] + Matriz2[i][j]);
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            cout<<Matriz[i][j] + Matriz2[i][j] << " ";
+        }
+        cout<<"\n";
     }
-    printf("\n");
-}
 
-}
-
-void multiplicar(int lm1, int cm1, int lm2, int cm2){
-int valor = 0;
-
-int mat1[lm1][cm1];
-int mat2[lm2][cm2];
-
-for (int i = 0; i < lm1; i++){
-    for (int j = 0; j < cm1; j++){
-        valor += 1;
-        printf("Digite o valor %d da matriz 1: ", valor);
-        scanf("%d", &mat1[lm1][cm1]);
     }
-}
+    void multiplicar(int lm1, int cm1, int lm2, int cm2){
+    int valor = 0;
 
-valor = 0;
-for (int i = 0; i < lm2; i++){
-    for (int j = 0; j < cm2; j++){
-        valor += 1;
-        printf("Digite o valor %d da matriz 2: ", valor);
-        scanf("%d", &mat2[lm2][cm2]);
+    int mat1[lm1][cm1];
+    int mat2[lm2][cm2];
+    int receptor[lm1][cm2];
+
+    for (int i = 0; i < lm1; i++){
+        for (int j = 0; j < cm1; j++){
+            valor += 1;
+            cout<<"Digite o valor "<< valor <<" da matriz 1: ";
+            cin>>mat1[lm1][cm1];
+        }
     }
-}
 
-    // Onde acontecera a multiplicação
-
-}
-
-int main(){
-    int i, linhasMat1, linhasMat2, colMat1, colMat2, r;
-    printf("Bem vindo a calculadora de Matrizes O que voce gostaria de fazer? \n");
-    while (true){
-        printf("1: Somar e Subtrair Matrizes \n");
-        printf("2: Multiplicar Matrizes \n");
-        printf("0: Sair do Programa \n");
-        scanf("%d", &i);
-        switch (i){
-            case (1):
-                printf("\nQual e a dimensao das matrizes a serem somadas? \n");
-                printf("Quantidade de Colunas: ");
-                scanf("%d", &colMat1);
-                printf("Quantidade de Linhas: ");
-                scanf("%d", &linhasMat1);
-                somar(colMat1, linhasMat1);
-                break;
-            
-            case (2):
-                r = 0;
-                while (r == 0){
-                    printf("\nQual e a dimensao das matrizes a serem multiplicadas?");
-                    printf("Quantidade de Linhas da Matriz 1: ");
-                    scanf("%d", &linhasMat1);
-                    printf("Quantidade de Colunas da Matriz 1: ");
-                    scanf("%d", &colMat1);
-                    printf("Quantidade de Linhas da Matriz 2: ");
-                    scanf("%d", &linhasMat2);
-                    printf("Quantidade de Colunas da Matriz 2: ");
-                    scanf("%d", &colMat2);
-                    if (colMat1 == linhasMat2){
-                        r = 1;
-                        multiplicar(linhasMat1, colMat1, linhasMat2, colMat2);
-                        
-                    } else {
-                        printf("O numero de colunas da primeira matriz deve equivaler ao numero de linhas da segunda matriz");
-                    }
+    valor = 0;
+    for (int i = 0; i < lm2; i++){
+        for (int j = 0; j < cm2; j++){
+            valor += 1;
+            cout<<"Digite o valor "<< valor <<" da matriz 2: ";
+            cin>>mat2[lm2][cm2];
+        }
+    }
+        // dessa parte pra frente de multi nao funfa
+        for (int i = 0; i < lm1; i++){
+            for (int j = 0; j < cm2; j++){
+                for (int k = 0; k < cm1; k++){
+                    receptor[i][j] += mat1[lm1][k] * mat2[k][cm2];
+                    printf("%d \n", receptor[i][j]);
                 }
-                break;
-            case (0):
-                printf("Tchau!");
-                return 0;
-                break;
+            }
+        }
 
-            default:
-                printf("Erro!");
-                break;
-        } 
+        for (int i = 0; i < lm1; i++){
+            for (int j = 0; j < cm2; j++){
+                printf("%d  ", receptor[i][j]);
+            }    
+            printf("\n");
+        }
+
+
     }
-}
+
+    int main(){
+        int i, linhasMat1, linhasMat2, colMat1, colMat2, r;
+        cout<<"Bem vindo a calculadora de Matrizes O que voce gostaria de fazer? \n";
+        while (true){
+            cout<<"1: Somar e Subtrair Matrizes \n";
+            cout<<"2: Multiplicar Matrizes \n";
+            cout<<"0: Sair do Programa \n";
+            cin>>i;
+            switch (i){
+                case (1):
+                    cout<<"\nQual e a dimensao das matrizes a serem somadas? \n";
+                    cout<<"Quantidade de Colunas: ";
+                    cin>>colMat1;
+                    cout<<"Quantidade de Linhas: ";
+                    cin>>linhasMat1;
+                    somar(colMat1, linhasMat1);
+                    break;
+                
+                case (2):
+                    r = 0;
+                    while (r == 0){
+                        cout<<"\nQual e a dimensao das matrizes a serem multiplicadas?";
+                        cout<<"Quantidade de Linhas da Matriz 1: ";
+                        cin>>linhasMat1;
+                        cout<<"Quantidade de Colunas da Matriz 1: ";
+                        cin>>colMat1;
+                        cout<<"Quantidade de Linhas da Matriz 2: ";
+                        cin>>linhasMat2;
+                        cout<<"Quantidade de Colunas da Matriz 2: ";
+                        cin>>colMat2;
+                        if (colMat1 == linhasMat2){
+                            r = 1;
+                            multiplicar(linhasMat1, colMat1, linhasMat2, colMat2);
+                            
+                        } else {
+                            cout<<"O numero de colunas da primeira matriz deve equivaler ao numero de linhas da segunda matriz";
+                        }
+                    }
+                    break;
+
+                case (0):
+                    cout<<"Tchau!";
+                    return 0;
+                    break;
+
+                default:
+                    cout<<"Erro!";
+                    break;
+            } 
+        }
+    }
 
