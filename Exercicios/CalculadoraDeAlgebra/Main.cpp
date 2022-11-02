4926c2bff8489c82a27593be21b969b337958be6
Eric(a) Hoffmann Braga <hero.b.braga@gmail.com>
2022-11-02 17:28:20 -0300

    refatorização de CalcAlg e Criação de arq Vetores

--- Exercicios/CalculadoraDeAlgebra/Main.cpp
+++ Exercicios/CalculadoraDeAlgebra/Main.cpp
@@ -8,9 +8,16 @@ using namespace std;
 
     int Matriz[n][m];
     int Matriz2[n][m];
+    int Resultante[n][m];
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            Resultante[i][j] = 0;
+        }
+    }
 
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
             valor += 1;
             cout<<"Digite o valor "<< valor <<" da matriz 1: ";
             cin>>Matriz[i][j];
@@ -20,21 +27,27 @@ using namespace std;
     cout<<"\n";
 
     valor = 0;
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
             valor += 1;
             cout<<"Digite o valor "<< valor <<" da matriz 2: ";
             cin>>Matriz2[i][j];
         }
     }
 
-    cout<<"\n";
-
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            cout<<Matriz[i][j] + Matriz2[i][j] << " ";
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            Resultante[i][j] = Matriz[i][j] + Matriz2[i][j];
         }
-        cout<<"\n";
+    }
+
+    cout<<"\nResultado:\n";
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cout<<Resultante[i][j]<<" ";
+        }
+        cout<<endl;
     }
 
     }
@@ -64,8 +77,6 @@ using namespace std;
 
     cout<<"\n";
 
-        // dessa parte pra frente de multi nao funfa
-
         for (int i = 0; i < lm1; i++){
             for (int j = 0; j < cm2; j++){
                 receptor[i][j] = 0;
@@ -76,64 +87,85 @@ using namespace std;
         for (int i = 0; i < lm1; i++){
             for (int j = 0; j < cm2; j++){
                 for (int k = 0; k < cm1; k++){
-                    cout<<"Impressao " << k+1 << " linha " << i+1 << " coluna " << j+1 << "\n";
-                    cout<<receptor[i][j];
-                    cout<<"\n Fora da matriz: \n";
-                    cout<<mat1[i][k] * mat2[k][j] << "\n";
                     receptor[i][j] += mat1[i][k] * mat2[k][j];
-                    
                 }
             }
         }
 
+    cout<<"Resultado:\n";
         for (int i = 0; i < lm1; i++){
             for (int j = 0; j < cm2; j++){
                 cout<<receptor[i][j] << " ";
             }    
             cout<<"\n";
         }
+    cout<<"\n";
 
+    }
+    void fazerCalculo(int Tipo){
+        cout<<"\nMonte suas matrizes \n";
+
+        int linhasM1, linhasM2, colunasM1, colunasM2;
+        bool seguir;
+        switch (Tipo)
+        {
+        case 1:
+            // Somar
+            cout<<"\nDimensoes das matrizes para soma:\n";
+            cout<<"Quantidade de linhas: ";
+            cin>>linhasM1;
+            cout<<"Quantidade de colunas: ";
+            cin>>colunasM1;
+            somar(linhasM1, colunasM1);
+            break;
+        case 2:
+            // Multiplicar
+            cout<<"\nDimensoes para as matrizes da multiplicacao:\n\n";
+            seguir = false;
+            while (seguir == false)
+            {
+                cout<<"Linhas da Matriz 1: ";
+                cin>>linhasM1;
+                cout<<"Colunas da Matriz 1: ";       
+                cin>>colunasM1;
+                cout<<"Linhas da Matriz 2: ";
+                cin>>linhasM2;
+                cout<<"Colunas da Matriz 2: ";       
+                cin>>colunasM2;
+                if (colunasM1 == linhasM2){
+                    seguir = true;
+                }else if (colunasM1 != linhasM2 || linhasM1 < 1 || linhasM2 < 1 || colunasM1 < 1 || colunasM2 < 1){
+                    cout<<"\nDimensoes invalidas:\nPara que as matrizes sejam multiplicaveis as Colunas da matriz 1 devem equivaler as Linhas da matriz 2\nTente Novamente\n";
+                }
+            }
+            cout<<"\n";
+            multiplicar(linhasM1, colunasM1, linhasM2, colunasM2);
+            break;
+            
+        default:
+            cout<<"Erro!";
+            break;
+        }
 
     }
 
     int main(){
-        int i, linhasMat1, linhasMat2, colMat1, colMat2, r;
-        cout<<"Bem vindo a calculadora de Matrizes O que voce gostaria de fazer? \n";
+        int i;
+        cout<<"Bem vindo a calculadora de Matrizes\n";
         while (true){
+            cout<<"O que voce deseja fazer?\n";
             cout<<"1: Somar e Subtrair Matrizes \n";
             cout<<"2: Multiplicar Matrizes \n";
             cout<<"0: Sair do Programa \n";
+            cout<<"Escolha: ";
             cin>>i;
             switch (i){
                 case (1):
-                    cout<<"\nQual e a dimensao das matrizes a serem somadas? \n";
-                    cout<<"Quantidade de Colunas: ";
-                    cin>>colMat1;
-                    cout<<"Quantidade de Linhas: ";
-                    cin>>linhasMat1;
-                    somar(colMat1, linhasMat1);
+                    fazerCalculo(1);
                     break;
                 
                 case (2):
-                    r = 0;
-                    while (r == 0){
-                        cout<<"\nQual e a dimensao das matrizes a serem multiplicadas? \n";
-                        cout<<"Quantidade de Linhas da Matriz 1: ";
-                        cin>>linhasMat1;
-                        cout<<"Quantidade de Colunas da Matriz 1: ";
-                        cin>>colMat1;
-                        cout<<"Quantidade de Linhas da Matriz 2: ";
-                        cin>>linhasMat2;
-                        cout<<"Quantidade de Colunas da Matriz 2: ";
-                        cin>>colMat2;
-                        if (colMat1 == linhasMat2){
-                            r = 1;
-                            multiplicar(linhasMat1, colMat1, linhasMat2, colMat2);
-                            
-                        } else {
-                            cout<<"O numero de colunas da primeira matriz deve equivaler ao numero de linhas da segunda matriz";
-                        }
-                    }
+                    fazerCalculo(2);
                     break;
 
                 case (0):
@@ -147,4 +179,3 @@ using namespace std;
             } 
         }
     }
-    //teste
